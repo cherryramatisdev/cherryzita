e4d936694ba2ca07287c6cdbdaae4219dc22e7d1
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-28 12:15:58 -0300

    Add binding to switch between sessions

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -42,8 +42,8 @@
       bind -n M-C-k resize-pane -U 5
       bind -n M-C-l resize-pane -R 5
 
-      # Open terminal on current path
-      bind -n M-t display-popup -E -w 90% -h 90% -d '#{pane_current_path}' "fish"
+      # Switch between sessions
+      bind-key -n M-o run-shell "tmux neww tmuxsessionizer"
       '';
   };
 }
